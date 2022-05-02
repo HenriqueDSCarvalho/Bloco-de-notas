34d1e73876fdd8ac891899b7e25c54c90a9d6312
HenriqueDSCarvalho <h96352828@gmail.com>
2022-05-01 21:27:59 -0300

    icons added

--- screns/Home.js
+++ screns/Home.js
@@ -1,23 +1,27 @@
-import {View, Text, StyleSheet, Button} from 'react-native';
+import { View, Text, StyleSheet, Button, TouchableOpacity } from 'react-native';
+import React from 'react';
+
+import { AntDesign } from '@expo/vector-icons';
 
 
-export default function Home({ navigation }){
-    return(
+export default function Home({ navigation }) {
+    return (
         <View>
-            <Text>
-                Home
-            </Text>
             <Button
-            title='navigate'
-            onPress={()=> navigation.navigate("Notas")}
+                title='navigate'
+                onPress={() => navigation.navigate("Notas")}
             />
+
+            <AntDesign name="addfile" size={24} color="black" />
+
+
 
         </View>
     );
 }
 
 const styles = StyleSheet.create({
-    container:{
+    container: {
 
     }
 
--- screns/Notas.js
+++ screns/Notas.js
@@ -4,9 +4,6 @@ import {View, Text, StyleSheet, Button} from 'react-native';
 export default function Notas({ navigation }){
     return(
         <View>
-            <Text>
-                Notas
-            </Text>
             <Button
             title='navigate'
             onPress={()=> navigation.navigate("Home")}
